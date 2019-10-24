686ddf8e59e7d8d2aba5313ae2c5c87a62d065cb
Joseph Waters <46896228+JoeWaters1980@users.noreply.github.com>
2019-10-24 11:56:24 +0100

    upto Q8

---
+++ Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_CA1
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            // a standard list construction
+            List<Player> players = new List<Player>();
+
+            Player player1 = new Player(1, "Millie", 0);
+            Player player2 = new Player(2, "Katie", 0);
+            Player player3 = new Player(3, "Kevin", 0);
+            Player player4 = new Player(4, "Conor", 0);
+            Player player5 = new Player(5, "Pauline", 0);
+
+            // adding to the list
+            players.Add(player1);
+            players.Add(player2);
+            players.Add(player3);
+            players.Add(player4);
+            players.Add(player5);
+
+
+            Console.WriteLine(player1.ToString()+"\n"+
+                player2.ToString()+"\n"+
+                player3.ToString()+"\n"+
+                player4.ToString()+ "\n"+
+                player5.ToString());
+        }
+
+      
+
+    }
+   
+}
---
+++ Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_CA1
+{
+    class Player
+    {
+        private readonly int _playerID;
+
+        public int PlayerID
+        {
+            get { return _playerID; }
+            
+        }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+
+       public Player(int playerID,string playerName, int score)
+       {
+            _playerID= playerID;
+             PlayerName= playerName;
+            Score=score;
+       }
+
+        public override string ToString()
+        {
+
+            return string.Format("Player ID : {0}  Player Name : {1} Score : {2}", PlayerID, PlayerName, Score);
+        }
+
+        public int IncreaseScore(int _score)
+        {
+            Score+= _score;
+
+            return _score;
+        }
+    }
+
+}
